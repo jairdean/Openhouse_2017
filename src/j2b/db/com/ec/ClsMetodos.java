96bbcb0d501135f166188206f181f0e885160380
jairdean <jairdean@hotmail.com>
2017-04-29 11:55:38 -0500

    login

---
+++ src/j2b/db/com/ec/ClsMetodos.java
@@ -0,0 +1,36 @@
+package j2b.db.com.ec;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ClsMetodos {
+	public boolean ComprobarLoguin(String usuario, String clave){
+		boolean t = false;
+		ClsConexion obj = new ClsConexion();
+		ResultSet rs=null;
+		String sql;
+		byte[] imgBytes3;
+		sql = "Select * from tb_usuarios where id_usuario='"+usuario+"' and clave='"+clave+"';";
+		//sql2="Select tb_usuarios.id_usuario,nombre,apellido,clave,correo,direccion,celular,perfil,tb_activos_usuarios.estado from tb_usuarios,tb_activos_usuarios where tb_usuarios.id_usuario='"+usuario+"' and tb_usuarios.clave='"+clave+"' and tb_usuarios.id_usuario=tb_activos_usuarios.id_fk_usuario and tb_activos_usuarios.estado=true"+" group by tb_usuarios.id_usuario,nombre,apellido,clave,correo,direccion,celular,perfil,tb_activos_usuarios.estado;";
+		try {
+			rs = obj.Consulta(sql);
+			while(rs.next()){
+				nombre = rs.getString(2);
+				t=true;
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		System.out.println(t);
+		System.out.println(sql);
+		
+		try {
+			rs.close();
+			obj.getConexion().close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return t;
+	}
+}
