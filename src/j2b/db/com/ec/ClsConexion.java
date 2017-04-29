e0e2a1250ed6aac6f8ae1b8b03a8504a107ed2bd
jairdean <jairdean@hotmail.com>
2017-04-29 11:10:42 -0500

    subiendo libreria

---
+++ src/j2b/db/com/ec/ClsConexion.java
@@ -0,0 +1,150 @@
+package j2b.db.com.ec;
+import java.sql.*;
+/*
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+*/
+
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+public class ClsConexion {
+	
+	private Connection con;
+	private Statement St;
+	private ResultSet rs;
+	private String driver;
+	private String user;
+	private String pwd;
+	private String cadena;
+	
+	
+	public String getDriver()
+	{
+		return this.driver;
+	}
+	String getUser()
+	{
+		return this.user;
+	}
+	String getPwd()
+	{
+		return this.pwd;
+	}
+	String getCadena()
+	{
+		return this.cadena;
+	}
+	public Connection getConexion()
+	{ 
+		return this.con; 
+	}
+	
+	public ClsConexion() {
+		
+		this.driver ="org.postgresql.Driver";
+		this.user="postgres";
+		this.pwd="jairo";
+		this.cadena="jdbc:postgresql://localhost:5432/db_openhouse2017";
+		
+		this.con=this.crearConexion();
+		
+	}
+	
+	Connection crearConexion(){
+		try {
+			Class.forName("org.postgresql.Driver");
+			}
+			catch (ClassNotFoundException e) {
+			
+			}
+		
+		try{
+			Class.forName(getDriver()).newInstance();
+			Connection con=DriverManager.getConnection(getCadena(),getUser(),getPwd());
+			return con;
+		}
+	catch(Exception ee)
+	{
+		System.out.println("Error: " + ee.getMessage());
+		return null;
+	}
+	}
+	
+
+	
+	// C O N E X I O N . . . . ... M Y S Q L
+	/*
+public ClsConexion() {
+		
+		this.driver ="org.postgresql.Driver";
+		this.user= "root";
+		this.pwd="jairo";
+		this.cadena="jdbc:mysql://localhost:3306/daniel";
+		//this.cadena="jdbc:postgresql://localhost:5432/bd_imagen";
+		
+		this.con=this.crearConexion();
+		
+	}
+	
+	Connection crearConexion(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			}
+			catch (Exception e) {
+			
+			}
+		
+		try{
+			Class.forName(getDriver()).newInstance();
+			Connection con=(Connection) DriverManager.getConnection(getCadena(),getUser(),getPwd());
+			return con;
+		}
+	catch(Exception ee)
+	{
+		System.out.println("Error: " + ee.getMessage());
+		return null;
+	}
+	}
+	*/
+
+	public String Ejecutar(String sql)
+	{
+	String error="";
+	try
+	{
+	St=(Statement) getConexion().createStatement();
+	St.execute(sql);
+	error="Datos insertados";
+	}
+	catch(Exception ex)
+	{
+	error = ex.getMessage();
+	}
+	return(error);
+	}
+
+
+
+	public ResultSet Consulta(String sql)
+	{
+	String error="";
+	ResultSet reg=null;
+	
+	try
+	{
+	St=(Statement) getConexion().createStatement();
+	reg=St.executeQuery(sql);
+	
+	
+	}
+	catch(Exception ee)
+	{
+	error = ee.getMessage();
+	}
+	return(reg);
+
+	
+	}
+}
